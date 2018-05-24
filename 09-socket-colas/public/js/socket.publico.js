1eb3312a8c7c76787c4879644f16f5674decc52d
devcr <crss5676@gmail.com>
2018-05-23 18:23:21 -0700

    Se termino practica app de colas

--- 09-socket-colas/public/js/socket.publico.js
+++ 09-socket-colas/public/js/socket.publico.js
@@ -1,7 +1,36 @@
 
 var socket = io ();
 
+var lblTicket1 = $('#lblTicket1');
+var lblTicket2 = $('#lblTicket2');
+var lblTicket3 = $('#lblTicket3');
+var lblTicket4 = $('#lblTicket4');
+
+var lblEscritorio1 = $('#lblEscritorio1');
+var lblEscritorio2 = $('#lblEscritorio2');
+var lblEscritorio3 = $('#lblEscritorio3');
+var lblEscritorio4 = $('#lblEscritorio4');
+
+var lblTickets = [ lblTicket1, lblTicket2, lblTicket3, lblTicket4];
+var lblEscritorios = [lblEscritorio1, lblEscritorio2, lblEscritorio3,lblEscritorio4];
+
 
 socket.on('ticketActual', function(data){
-  console.log(data);
+  //console.log(data);
+
+  var audio = new Audio ('audio/new-ticket.mp3');
+  audio.play();
+  actualizaHTML( data.ultimos4 );
 });
+
+
+function actualizaHTML( ultimos4 ){
+
+  for( i = 0; i < ultimos4.length; i++ ){
+
+    lblTickets[i].text('Ticket ' + ultimos4[i].numero);
+    lblEscritorios[i].text('Escritorio ' + ultimos4[i].escritorio);
+
+  }
+
+}
