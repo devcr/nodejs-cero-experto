78f70f1fc03c8da0173461da535f575c82898c53
devcr <crss5676@gmail.com>
2018-05-30 18:32:49 -0700

    Se termino app de chat

--- 10-socket-chat/public/js/socket-chat.js
+++ 10-socket-chat/public/js/socket-chat.js
@@ -18,7 +18,8 @@ socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
 
 });
@@ -41,13 +42,16 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-    console.log(personas);
+    //console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
--- 10-socket-chat/server/sockets/socket.js
+++ 10-socket-chat/server/sockets/socket.js
@@ -23,15 +23,17 @@ io.on('connection', (client) => {
 
     client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
 
-    callback(personas);
+    //notifica en la sala que un usuario se unio
+    client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unio`));
 
-    //console.log(usuario);
+    //callback(personas);   30may2018
+    callback(usuarios.getPersonasPorSala(data.sala));
 
   });
 
 
   // para enviar mensaje a todos
-  client.on('crearMensaje', (data) =>{
+  client.on('crearMensaje', (data, callback) =>{
 
     let persona = usuarios.getPersona(client.id);
 
@@ -39,13 +41,15 @@ io.on('connection', (client) => {
     let mensaje = crearMensaje( persona.nombre, data.mensaje );
     client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
 
+    callback(mensaje);
+
   });
 
   client.on('disconnect', ()=>{
     let borrado = usuarios.borrarPersona(client.id);
 
     // Notifico a los clinetes que tal usuario abano el entrarChat
-    client.broadcast.to(borrado.sala).emit('crearMensaje', crearMensaje('Adminstrador', `${borrado.nombre} abandono el chat`));
+    client.broadcast.to(borrado.sala).emit('crearMensaje', crearMensaje('Administrador', `${borrado.nombre} abandono el chat`));
 
     // regresa lista a los clientes la lista de usuarios aun conectados
     client.broadcast.to(borrado.sala).emit('listaPersona', usuarios.getPersonasPorSala(borrado.sala));
